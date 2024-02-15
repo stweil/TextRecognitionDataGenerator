stweil/TextRecognitionDataGenerator

commit 74b1869e83f63c91a81c09cce0e388faa081761f

Add support for Japanese

trdg/data_generator.py
@@ -1,7 +1,7 @@
 import os
 import random as rnd
 
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageStat
 
 from trdg import computer_text_generator, background_generator, distorsion_generator
 
@@ -173,6 +173,26 @@ class FakeTextDataGenerator(object):
         background_mask = Image.new(
             "RGB", (background_width, background_height), (0, 0, 0)
         )
+
+        ##############################################################
+        # Comparing average pixel value of text and background image #
+        ##############################################################
+        try:
+            resized_img_st = ImageStat.Stat(resized_img, resized_mask.split()[2])
+            background_img_st = ImageStat.Stat(background_img) 
+
+            resized_img_px_mean = sum(resized_img_st.mean[:2]) / 3
+            background_img_px_mean = sum(background_img_st.mean) / 3
+
+            if abs(resized_img_px_mean - background_img_px_mean) < 15:
+                print("value of mean pixel is too similar. Ignore this image")
+
+                print("resized_img_st \n {}".format(resized_img_st.mean))
+                print("background_img_st \n {}".format(background_img_st.mean))
+
+                return
+        except Exception as err:
+            return
 
         #############################
         # Place text with alignment #

MANIFEST.in
@@ -2,6 +2,7 @@ include README.md
 include LICENSE
 include trdg/fonts/latin/*
 include trdg/fonts/cn/*
+include trdg/fonts/ja/*
 include trdg/images/*
 include trdg/dicts/*
 include trdg/texts/*

tests.py
@@ -1095,6 +1095,13 @@ class DataGenerator(unittest.TestCase):
 
         self.assertTrue(all([l in cn_chars for l in s]))
 
+    def test_generate_japanese_string(self):
+        s = create_strings_randomly(1, False, 1, True, False, False, "ja")[0]
+
+        ja_chars = [chr(i) for i in range(12288, 12543)] + [chr(i) for i in range(65280, 65519)] + [chr(i) for i in range(19968, 40908)]
+
+        self.assertTrue(all([l in ja_chars for l in s]))
+
     def test_generate_data_with_white_background(self):
         background_generator.plain_white(64, 128).convert("RGB").save(
             "tests/out/white_background.jpg"

trdg/run.py
@@ -51,7 +51,7 @@ def parse_arguments():
         "--language",
         type=str,
         nargs="?",
-        help="The language to use, should be fr (French), en (English), es (Spanish), de (German), ar (Arabic), cn (Chinese), or hi (Hindi)",
+        help="The language to use, should be fr (French), en (English), es (Spanish), de (German), ar (Arabic), cn (Chinese), ja (Japanese) or hi (Hindi)",
         default="en",
     )
     parser.add_argument(

trdg/string_generator.py
@@ -96,6 +96,23 @@ def create_strings_randomly(length, allow_variable, count, let, num, sym, lang):
             pool += "".join(
                 [chr(i) for i in range(19968, 40908)]
             )  # Unicode range of CHK characters
+        elif lang == "ja":
+            pool += "".join(
+                [chr(i) for i in range(12288, 12351)]
+            )   # unicode range for japanese-style punctuation
+            pool += "".join(
+                [chr(i) for i in range(12352, 12447)]
+            )   # unicode range for Hiragana
+            pool += "".join(
+                [chr(i) for i in range(12448, 12543)]
+            )   # unicode range for Katakana
+            pool += "".join(
+                [chr(i) for i in range(65280, 65519)]
+            )   # unicode range for Full-width roman characters and half-width katakana 
+            pool += "".join(
+                [chr(i) for i in range(19968, 40908)]
+            )   # unicode range for common and uncommon kanji
+            # https://stackoverflow.com/questions/19899554/unicode-range-for-japanese
         else:
             pool += string.ascii_letters
     if num:
@@ -104,6 +121,9 @@ def create_strings_randomly(length, allow_variable, count, let, num, sym, lang):
         pool += "!\"#$%&'()*+,-./:;?@[\\]^_`{|}~"
 
     if lang == "cn":
+        min_seq_len = 1
+        max_seq_len = 2
+    elif lang == "ja":
         min_seq_len = 1
         max_seq_len = 2
     else:

commit 36fc57d14bcf89ed876593fac79c8a91f4af6d86

Fix regression for handwritten text generation
The handwritten text generation no longer created any output
because of a ZeroDivisionError exception.

This commit fixes that regression.

Fixes: 74b1869e83f63c91 ("Add support for Japanese")
Signed-off-by: Stefan Weil <sw@weilnetz.de>

trdg/data_generator.py
@@ -195,6 +195,9 @@ class FakeTextDataGenerator(object):
                 print("background_img_st \n {}".format(background_img_st.mean))
 
                 return
+        except ZeroDivisionError:
+            # resized_img_st.mean could not be calculated
+            pass
         except Exception as err:
             return
 